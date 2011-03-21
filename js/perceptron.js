e90a2a9c0ec36a7b401a9433d890ed0c26202498
John Doe <eyospace@googlemail.com>
2011-03-21 21:33:57 +0100

    Optimizing, Callbacks and Canvas

--- js/perceptron.js
+++ js/perceptron.js
@@ -1,120 +1,141 @@
-// Debugging tools
+var log = (function(d) {
+    var output = d.getElementById('assert'); 
+    
+    return function(content) {
+        var li = d.createElement('li');
+        li.className = 'log';
+        li.appendChild( d.createTextNode( content ) );
+    
+        output.appendChild(li);
+    }; 
+})(document);
 
-var output = document.getElementById('assert');
 
-function assert( outcome, description ) {
-    var li = document.createElement('li');
-    li.className = outcome ? 'pass' : 'fail';
-    li.appendChild( document.createTextNode( description ) );
 
-    output.appendChild(li);
+var Neuron = function(weights, threshold, learnRate) {
+    this.weights      = weights || [];
+    this.threshold    = threshold || 0;
+    this.learnRate    = learnRate || 0.5;
+    this.bias         = 1;
+    this.biasWeight   = 1;
+    this.nbInputs     = 0;
+    this.sigmoid      = function(x) {
+        return 1/ (1 - Math.exp(-1 * x));
+    };
+    
+    this.init = function(nb, callback) {
+        if( this.weights.length == 0) {
+            this.initWeights(nb, callback);
+        }
+    };
+    
+    this.initWeights = function(nbInputs, callback) {
+        if( this.weights.length == 0 ) {
+            for( var i = 0; i < nbInputs; i++ ) {
+                this.weights[i] = parseInt( ( Math.random() * 100 ) / 10 );
+            }    
+        }
+        this.nbInputs = nbInputs;
+        this.weights.push( this.biasWeight ); 
+        
+        // Callback
+        typeof callback === "undefined" || callback(this.weights, this.nbInputs);
+        
+        // Chaining
+        return this;
+    };
+    
+    this.send = function(inputs, callback) {
+        this.init(inputs.length);
+        
+        inputs = inputs.slice(0, this.nbInputs);
+        
+        // Weighted sum of inputs
+        var weightedSum = 0;
+        for( var i = 0; i < inputs.length; i++) {
+            weightedSum += inputs[i] * this.weights[i];
+        }
+        weightedSum += this.bias * this.biasWeight;
+        
+        // Callback
+        typeof callback === "undefined" || callback(inputs, weightedSum);
+        
+        return weightedSum >= this.threshold ? 1 : 0;
+        // return Math.round( this.sigmoid(weightedSum) );
+    };
+    
+    this.learn = function(data) {
+        this.init(data[0][0].length);
+        
+        for ( var i = 0; i < data.length; i++ ) {
+            currentOutput = this.send(data[i][0]);
+            providedOutput = data[i][1];
+            
+            for( var j = 0; j < this.nbInputs; j++) {
+                this.weights[j] += this.learnRate * (providedOutput - currentOutput) * data[i][0][j];
+                this.biasWeight = this.biasWeight + (providedOutput - currentOutput);
+            }
+        }
+        
+        return this;
+    };
+};
+
+// Network test
+var sample = [
+        [[-2, -3], 0],
+        [[-1, -1], 0],
+        [[-1, 0], 0],
+        [[-2, 1], 0],
+        [[0, 1], 0],
+        [[0, 2], 0],
+        [[1, 3], 0],
+        [[2, 5], 0],
+        [[-2, -5], 1],
+        [[-1, -3], 1],
+        [[0, -1], 1],
+        [[2, -2], 1],
+        [[1, 1], 1],
+        [[2, 3], 1],
+        [[2, 1], 1],
+    ],
+    test = [-5, 5],
+    neuron = new Neuron().learn(sample);
+
+var output = neuron.send(test, log);
+log( 'Output : ' + output );
+
+
+// Canvas graph
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+    
+// Axes
+ctx.strokeStyle = "#ddd";
+ctx.moveTo(200, 0);
+ctx.lineTo(200, 400);
+ctx.stroke();
+ctx.moveTo(0, 200);
+ctx.lineTo(600, 200);
+ctx.stroke();
+    
+// Separate
+ctx.moveTo(100, 0);
+ctx.lineTo(300, 400);
+ctx.stroke();
+
+// Samples
+for(var i = 0, len = sample.length; i < len; i++) {
+    ctx.fillStyle = sample[i][1] === 1 ? "#FF1C0A" : "#00A308";
+    ctx.beginPath();
+    ctx.arc((sample[i][0][0]+20)*10, (sample[i][0][1]+20)*10, 1, 0, Math.PI*2, true); 
+    ctx.closePath();
+    ctx.fill();
 }
 
-function log( content ) {
-    var li = document.createElement('li');
-    li.className = 'log';
-    li.appendChild( document.createTextNode( content ) );
-
-    output.appendChild(li);
-}
-
-function concatObject(obj) {
-  str='';
-  for(prop in obj)
-  {
-    str+=prop + " value :"+ obj[prop]+"\n";
-  }
-  return(str);
-}
-
-$(function() {
-
-	var Maths = function() {
-		this.sigmoid = function(x) {
-			return 1/ (1 - Math.exp(-1 * x));
-		};
-	};
-	
-    var Neuron = function(weights, threshold, learnRate) {
-        this.weights 	= weights || [];
-        this.threshold 	= threshold || 0;
-        this.learnRate 	= learnRate || 0.5;
-		this.bias 		= 1;
-		this.biasWeight = 1;
-        this.nbInputs 	= 0;
-		var maths = new Maths();
-		
-		this.init = function(nb) {
-			if( this.weights.length == 0) {
-				this.initWeights(nb);
-			}
-		};
-		
-		this.initWeights = function(nbInputs) {
-			if( this.weights.length == 0 ) {
-				for( var i = 0; i < nbInputs; i++ ) {
-					this.weights[i] = parseInt( ( Math.random() * 100 ) / 10 );
-				}	
-			}
-			this.nbInputs = nbInputs;
-			this.weights.push( this.biasWeight );
-			log( this.weights );
-			
-			return this;
-		};
-		
-		this.send = function(inputs) {
-			this.init(inputs.length);
-			
-			inputs = inputs.slice(0, this.nbInputs);
-			log( inputs );
-			
-			// Weighted sum of inputs
-			var weightedSum = 0;
-			for( var i = 0; i < inputs.length; i++) {
-				weightedSum += ( inputs[i]*this.weights[i] );
-			}
-			weightedSum += ( this.bias * this.biasWeight );
-			
-			// return weightedSum >= this.threshold ? 1 : 0;
-			return Math.round( maths.sigmoid(weightedSum) );
-		};
-		
-		this.learn = function(data) {
-			this.init(data[0][0].length);
-			
-			for ( var i = 0; i < data.length; i++ ) {
-				currentOutput = this.send(data[i][0]);
-				providedOutput = data[i][1];
-				
-				for( var j = 0; j < this.nbInputs; j++) {
-					this.weights[j] = this.weights[j] + ( this.learnRate * (providedOutput - currentOutput) * data[i][0][j] );
-					this.biasWeight = this.biasWeight + (providedOutput - currentOutput);
-				}
-			}
-			return this;
-		};
-	};
-	
-	var neuron = new Neuron();
-	neuron.learn([
-					[[-2, -3], 0],
-					[[-1, -2], 0],
-					[[-1, 0], 0],
-					[[-2, 1], 0],
-					[[0, 1], 0],
-					[[0, 2], 0],
-					[[1, 3], 0],
-					[[2, 5], 0],
-					[[-2, -5], 1],
-					[[-1, -3], 1],
-					[[0, -1], 1],
-					[[2, -2], 1],
-					[[0, 1], 1],
-					[[1, 1], 1],
-					[[2, 3], 1],
-					[[2, 1], 1],
-				 ]);
-				 
-	log( 'Output : ' +neuron.send([-10, 12]) );
-});
+// Test
+ctx.fillStyle = output === 1 ? "#FF1C0A" : "#00A308";
+ctx.beginPath();
+ctx.arc((test[0] +20)*10, (test[1] +20)*10, 2, 0, Math.PI*2, true); 
+ctx.closePath();
+ctx.fill();
